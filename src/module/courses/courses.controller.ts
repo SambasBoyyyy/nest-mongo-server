68d2d78cbaa726db18349499e29b0b1b4f7c593a
Samiul Basir <samiulbasirbhuiyan1234@gmail.com>
2024-02-10 00:54:09 +0600

    initiating courses with error

--- src/module/courses/courses.controller.ts
+++ src/module/courses/courses.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req } from '@nestjs/common';
 import { CoursesService } from './courses.service';
 
 @Controller('courses')
 export class CoursesController {
   constructor(private readonly coursesService: CoursesService) {}
+  
+  @Post('add-coursesByadmin')
+  async addCoursesToUser(@Req() req: Request, @Body() ChooseCourse: UserChooseCourse) {
+    const userId = req['userId'];
+    return await this.userService.addCourses(userId, ChooseCourse);
+  }
 }
