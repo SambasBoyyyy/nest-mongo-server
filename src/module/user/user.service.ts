04de66f2d93142cd7280f4a733b17e6b4a91680f
Samiul Basir <samiulbasirbhuiyan1234@gmail.com>
2024-02-14 22:28:18 +0600

    removed trash code

--- src/module/user/user.service.ts
+++ src/module/user/user.service.ts
@@ -77,34 +77,7 @@ export class UserService {
     }
   }
   
-  async login(payload: LoginUserDto): Promise<SignInResponse> {
 
-    let user = await this.userModel.findOne({ email: payload.email });
-    // if (!user || !(await bcrypt.compare(payload.password, user.password))) {
-    //   throw new BadRequestException('invalid email or password');
-    // }
-
-    if (!user || !((payload.password === user.password))) {
-      throw new BadRequestException('invalid email or password');
-    }
-
-    //  const token = await this.generateToken(user);
-    //  user.access_token = token.access_token;
-    //  user.refresh_token = token.refresh_token;
-    //  user = await this.userModel.save(user);
-    // user.image_url = getProfileImageUrl(user.image_url); 
-    const response: SignInResponse = {
-      id: user.id,
-      first_name: user.firstName,
-      last_name: user.lastName,
-      image_url: '',//user.image_url,
-      email: user.email,
-      access_token: 'user.access_token',
-      refresh_token: 'user.refresh_token',
-    };
-
-    return response;
-  }
 
   async update_user(_payload: UpdateUserDto): Promise<UpdateUserResponse> {
    
@@ -193,14 +166,6 @@ async deleteCourses(userId: string, RemoveCourse: UserRemoveCourse): Promise<any
     // handle user not found
     throw new Error('User not found');
   }
-
-  // Remove the courseIdsToDelete from user's courseID array
-  // user.courseID = RemoveCourse.courseIds.filter(courseId => !user.courseID.includes(courseId));
-
-  // // Save the updated user document
-  // await user.save();
-
-  // Delete the specified courses from the database
   await this.userModel.updateOne(
     { _id: user._id },
     { $pull: { courseID: { $in: RemoveCourse.courseIds } } }
