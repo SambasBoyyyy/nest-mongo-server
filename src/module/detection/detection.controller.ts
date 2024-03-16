86d89409fb548dbf39020e8ef904751a7655778b
Samiul Basir <samiulbasirbhuiyan1234@gmail.com>
2024-03-17 00:00:29 +0600

    deleted detection

--- src/module/detection/detection.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { DetectionService } from './detection.service';
-
-@Controller('detection')
-export class DetectionController {
-  constructor(private readonly detectionService: DetectionService) {}
-}
--- src/module/detection/detection.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DetectionService } from './detection.service';
-import { DetectionController } from './detection.controller';
-
-@Module({
-  controllers: [DetectionController],
-  providers: [DetectionService],
-})
-export class DetectionModule {}
